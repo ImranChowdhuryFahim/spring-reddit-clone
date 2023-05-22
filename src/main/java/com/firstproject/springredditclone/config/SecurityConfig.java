c89e028528700758cd29dc24e921c4556073de7c
Imran Chowdhury <imran.cuet.cse17@gmail.com>
2023-05-23 01:14:28 +0600

    completed user signup functionality

---
+++ src/main/java/com/firstproject/springredditclone/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.firstproject.springredditclone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic(httpBasic -> httpBasic.disable()).csrf(csrf -> csrf.disable()).cors(cors ->cors.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+}
